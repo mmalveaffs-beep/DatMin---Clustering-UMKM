f7138b19da61ebb886b5a40f19e0fa3568733bf8
DINDA <dinda@example.com>
2026-05-27 10:33:18 +0700

    add preprocessing dan PCA

---
+++ maincode.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+# In[ ]:
+
+
+df = pd.read_csv("Kompilasi_Data_1.xlsx")
+
+
+# In[ ]:
+
+
+df.shape
+
+
+# In[ ]:
+
+
+df.head
+
